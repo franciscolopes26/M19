09d38272fe70fd70e96d7cfa8004b7abeba1f914
francisco <franciscolopesr26@gmail.com>
2020-12-02 09:41:45 +0000

    ex1

---
+++ ex1 certo/ex1 certo/webapp.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="webapp.aspx.cs" Inherits="ex1_certo.webapp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txt1" runat="server"></asp:TextBox>
+            <asp:Button ID="btn1" runat="server" Text="Button" OnClick="btn1_Click" />
+        </div>
+        <asp:Label ID="lbl1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ex1 certo/ex1 certo/webapp.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ex1_certo
+{
+    public partial class webapp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            if(txt1.Text != "") 
+            {
+                lbl1.Text = "<h1>Bem vindo ao parque por favor retire o seu bilhete/a" + txt1.Text + "</h1>";
+            }
+            else
+            {
+                lbl1.Text = "<h1>Primeiro escreva o seu nome</h1>";
+            }
+            
+
+        }
+    }
+}
